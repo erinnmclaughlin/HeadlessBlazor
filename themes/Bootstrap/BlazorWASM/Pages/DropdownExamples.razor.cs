d1a0d3b1d45f05c0814e05c8d10c57a72bdf8c82
Erin McLaughlin <erinmclaughlin219@gmail.com>
2024-05-15 17:53:25 -0400

    update code example

--- themes/Bootstrap/BlazorWASM/Pages/DropdownExamples.razor.cs
+++ themes/Bootstrap/BlazorWASM/Pages/DropdownExamples.razor.cs
@@ -3,17 +3,18 @@ namespace BlazorWASM.Pages;
 public partial class DropdownExamples
 {
     private const string Example1CodeBlock = """
-        <Dropdown class="dropdown d-inline-block">
-            <DropdownTrigger class="btn btn-primary dropdown-toggle">Click Me</DropdownTrigger>
-            <DropdownItems class="dropdown-menu show">
-                <DropdownItem class="dropdown-item">Default</DropdownItem>
-                <DropdownItemButton class="dropdown-item" type="button">Button</DropdownItemButton>
-                <DropdownItemButton class="dropdown-item" type="button" disabled="true">Disabled Button</DropdownItemButton>
-                <DropdownItemButton class="dropdown-item" type="button"@onclick:stopPropagation="true">Button (Stop Propagation)</DropdownItemButton>
-                <DropdownItemLink class="dropdown-item" href="#">Link</DropdownItemLink>
-                <DropdownItemLink class="dropdown-item" href="#" @onclick:preventDefault="true">Link (Prevent Default)</DropdownItemLink>
-                <DropdownItemLink class="dropdown-item" href="#" @onclick:stopPropagation="true">Link (Stop Propagation)</DropdownItemLink>
-                <DropdownItemLink class="dropdown-item" href="#" OnClickPreventDefault="true" OnClickStopPropagation="true">Link (Prevent Default and Stop Propagation)</DropdownItemLink>
+        <Dropdown>
+            <DropdownOutsideClickBehavior />
+            <DropdownTrigger>Click Me</DropdownTrigger>
+            <DropdownItems>
+                <DropdownItem>Default</DropdownItem>
+                <DropdownItemButton>Button</DropdownItemButton>
+                <DropdownItemButton disabled="true">Disabled Button</DropdownItemButton>
+                <DropdownItemButton @onclick:stopPropagation="true">Button (Stop Propagation)</DropdownItemButton>
+                <DropdownItemLink>Link</DropdownItemLink>
+                <DropdownItemLink @onclick:preventDefault="true">Link (Prevent Default)</DropdownItemLink>
+                <DropdownItemLink @onclick:stopPropagation="true">Link (Stop Propagation)</DropdownItemLink>
+                <DropdownItemLink OnClickPreventDefault="true" OnClickStopPropagation="true">Link (Prevent Default and Stop Propagation)</DropdownItemLink>
             </DropdownItems>
         </Dropdown>
         """;
