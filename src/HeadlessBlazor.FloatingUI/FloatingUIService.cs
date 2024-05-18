d38d7acfe782dd996b15f36b43e7c799aab497d6
Erin McLaughlin <erinmclaughlin219@gmail.com>
2024-05-18 11:12:27 -0400

    playing around with floating ui

---
+++ src/HeadlessBlazor.FloatingUI/FloatingUIService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace HeadlessBlazor.FloatingUI;
+
+public class FloatingUIService
+{
+    public async Task<>
+
+    public async Task<IComputedPosition> ComputePositionAsync(ElementReference reference)
+    {
+
+    }
+}
+
+public interface IPlatform
+{
+
+}
+
+public interface IComputePositionConfiguration
+{
+    
+}
+
+public interface IComputedPosition
+{
+
+}
+
+public sealed record Placement(Side Side, Alignment? Alignment = null);
+
+public enum Alignment { Start, End }
+public enum Axis { X, Y }
+public enum Side { Top, Right, Bottom, Left }
+public enum Strategy { Absolute, Fixed }
+
+public sealed record Coords(int X, int Y);
+public sealed record Dimensions(float Width, float Height);
