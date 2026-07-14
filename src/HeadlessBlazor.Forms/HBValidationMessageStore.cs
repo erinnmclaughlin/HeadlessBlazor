d9cfce75cd0c92f46bccad63bfa61705adec132f
Erin McLaughlin <erinmclaughlin219@gmail.com>
2026-07-13 22:37:32 -0400

    added docblock comments; also renamed project

---
+++ src/HeadlessBlazor.Forms/HBValidationMessageStore.cs
@@ -0,0 +1,146 @@
+using Microsoft.AspNetCore.Components.Forms;
+using System.Linq.Expressions;
+
+namespace HeadlessBlazor;
+
+/// <summary>
+/// A strongly-typed wrapper around <see cref="ValidationMessageStore"/> that lets callers identify
+/// fields on <typeparamref name="TModel"/> via member-access expressions instead of raw field names.
+/// Owned by <see cref="HBEditContext{TModel}"/>.
+/// </summary>
+/// <typeparam name="TModel">The type of the model being validated.</typeparam>
+public class HBValidationMessageStore<TModel> where TModel : notnull
+{
+    private readonly EditContext _editContext;
+
+    /// <summary>
+    /// The underlying <see cref="ValidationMessageStore"/>. Created on first access.
+    /// </summary>
+    public ValidationMessageStore Store => field ??= new ValidationMessageStore(_editContext);
+
+    /// <summary>
+    /// Initializes a new <see cref="HBValidationMessageStore{TModel}"/> backed by the given edit context.
+    /// </summary>
+    /// <param name="editContext">The edit context to add and clear validation messages against.</param>
+    public HBValidationMessageStore(EditContext editContext)
+    {
+        _editContext = editContext;
+    }
+
+    /// <summary>
+    /// Adds a validation message for the field identified by <paramref name="fieldExpression"/>.
+    /// </summary>
+    /// <param name="fieldExpression">A member-access expression identifying the field, e.g. <c>m => m.Name</c>.</param>
+    /// <param name="errorMessage">The validation message to add.</param>
+    public void Add(Expression<Func<TModel, object>> fieldExpression, string errorMessage)
+    {
+        var fieldName = GetFieldName(fieldExpression);
+        Add(fieldName, errorMessage);
+    }
+
+    /// <summary>
+    /// Adds a validation message for the field with the given name.
+    /// </summary>
+    /// <param name="fieldName">The name of the field on <typeparamref name="TModel"/>.</param>
+    /// <param name="errorMessage">The validation message to add.</param>
+    public void Add(string fieldName, string errorMessage)
+    {
+        var fieldId = _editContext.Field(fieldName);
+        Add(fieldId, errorMessage);
+    }
+
+    /// <summary>
+    /// Adds a validation message for the given field.
+    /// </summary>
+    /// <param name="fieldIdentifier">The field to add the message for.</param>
+    /// <param name="errorMessage">The validation message to add.</param>
+    public void Add(FieldIdentifier fieldIdentifier, string errorMessage)
+    {
+        Store.Add(fieldIdentifier, errorMessage);
+    }
+
+    /// <summary>
+    /// Adds multiple validation messages for the field identified by <paramref name="fieldExpression"/>.
+    /// </summary>
+    /// <param name="fieldExpression">A member-access expression identifying the field, e.g. <c>m => m.Name</c>.</param>
+    /// <param name="errorMessages">The validation messages to add.</param>
+    public void AddRange(Expression<Func<TModel, object>> fieldExpression, IEnumerable<string> errorMessages)
+    {
+        var fieldName = GetFieldName(fieldExpression);
+        AddRange(fieldName, errorMessages);
+    }
+
+    /// <summary>
+    /// Adds multiple validation messages for the field with the given name.
+    /// </summary>
+    /// <param name="fieldName">The name of the field on <typeparamref name="TModel"/>.</param>
+    /// <param name="errorMessages">The validation messages to add.</param>
+    public void AddRange(string fieldName, IEnumerable<string> errorMessages)
+    {
+        var fieldId = _editContext.Field(fieldName);
+        AddRange(fieldId, errorMessages);
+    }
+
+    /// <summary>
+    /// Adds multiple validation messages for the given field.
+    /// </summary>
+    /// <param name="fieldIdentifier">The field to add the messages for.</param>
+    /// <param name="errorMessages">The validation messages to add.</param>
+    public void AddRange(FieldIdentifier fieldIdentifier, IEnumerable<string> errorMessages)
+    {
+        Store.Add(fieldIdentifier, errorMessages);
+    }
+
+    /// <summary>
+    /// Clears all validation messages in the store.
+    /// </summary>
+    public void Clear()
+    {
+        Store.Clear();
+    }
+
+    /// <summary>
+    /// Clears validation messages for the field identified by <paramref name="fieldExpression"/>.
+    /// </summary>
+    /// <param name="fieldExpression">A member-access expression identifying the field, e.g. <c>m => m.Name</c>.</param>
+    public void Clear(Expression<Func<TModel, object>> fieldExpression)
+    {
+        var fieldName = GetFieldName(fieldExpression);
+        Clear(fieldName);
+    }
+
+    /// <summary>
+    /// Clears validation messages for the field with the given name.
+    /// </summary>
+    /// <param name="fieldName">The name of the field on <typeparamref name="TModel"/>.</param>
+    public void Clear(string fieldName)
+    {
+        var fieldId = _editContext.Field(fieldName);
+        Clear(fieldId);
+    }
+
+    /// <summary>
+    /// Clears validation messages for the given field.
+    /// </summary>
+    /// <param name="fieldIdentifier">The field to clear messages for.</param>
+    public void Clear(FieldIdentifier fieldIdentifier)
+    {
+        Store.Clear(fieldIdentifier);
+    }
+
+    private static string GetFieldName(Expression<Func<TModel, object>> fieldExpression)
+    {
+        if (fieldExpression.Body is MemberExpression memberExpression)
+        {
+            return memberExpression.Member.Name;
+        }
+        else if (fieldExpression.Body is UnaryExpression unaryExpression && unaryExpression.Operand is MemberExpression memberOperand)
+        {
+            return memberOperand.Member.Name;
+        }
+        else
+        {
+            throw new ArgumentException("Invalid field expression", nameof(fieldExpression));
+        }
+    }
+}
